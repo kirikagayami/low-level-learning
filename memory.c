3612a12a8ad0c23c0149d82e579828b016164206
kirikagayami <kirikagayami@gmail.com>
2026-09-08 19:55:14 +0700

    Start exploring memory and pointers

---
+++ memory.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 42;
+	int *p = &x;
+
+	printf("x = %d\n", x);
+	printf("&x = %p\n", (void *)&x);
+
+	printf("p = %p\n", (void *)p);
+	printf("*p = %d\n", *p);
+
+	return 0;
+}

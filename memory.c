df364b9b7bb96162927852ee4c9f0978f5a8e82b
kirikagayami <kirikagayami@gmail.com>
2026-09-09 20:44:16 +0700

    Checkpoint comment

--- memory.c
+++ memory.c
@@ -3,13 +3,20 @@
 int main(void)
 {
 	int x = 42;
-	int *p = &x;
+	// int *p = &x;
+
+	// printf("x = %d\n", x);
+	// printf("&x = %p\n", (void *)&x);
+
+	// printf("p = %p\n", (void *)p);
+	// printf("*p = %d\n", *p);
 
 	printf("x = %d\n", x);
-	printf("&x = %p\n", (void *)&x);
-
-	printf("p = %p\n", (void *)p);
-	printf("*p = %d\n", *p);
+	// printf("size = %zu bytes\n", sizeof(x));
+	
+	// & operator is called the address-of-operator 
+	// x currently exists at virtual memory address, the address is different, depends on where the os put it in the memory
+	printf("address of x = %p\n", (void *)&x);
 
 	return 0;
 }

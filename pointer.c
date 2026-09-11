696826d750c49e0a82ee9160e85bbdd1627e30a0
kirikagayami <kirikagayami@gmail.com>
2026-09-11 17:49:10 +0700

    Learn pointer redirection and dereferencing

--- pointer.c
+++ pointer.c
@@ -4,9 +4,13 @@
 // p is the address of x
 // *p is go to the address stored inside p, and access what's there
 
+// p = changing the pointer itself
+// *p = changing/accessing the thing being pointed to
+
 int main(void)
 {
     int x = 42;
+    int y = 99;
     int *p = &x;
 
     printf("x = %d\n", x);
@@ -14,11 +18,27 @@ int main(void)
     printf("p = %p\n", (void *)p);
     printf("*p = %d\n", *p);
 
-    *p = 100;
+    p = &y;
+    *p = 500;
+    p = &x;
 
     printf("x = %d\n", x);
-    printf("*p = %d\n", *p);
+    printf("y = %d\n", y);
+    printf("address of y = %p\n", (void*)&y);
     printf("p = %p\n", (void *)p);
+    printf("*p = %d\n", *p);
+    
 
+    // *p = 100;
+
+    // printf("x = %d\n", x);
+    // printf("*p = %d\n", *p);
+    // printf("p = %p\n", (void *)p);
+    // printf("\n");
+
+    // printf("x = %d\n", x);
+    // printf("y = %d\n", y);
+    // printf("*p = %d\n", *p);
+    
     return 0;
 }

d58fb1eab0c9b36abec092c1fe438be4c6b92767
kirikagayami <kirikagayami@gmail.com>
2026-09-11 18:06:48 +0700

    Making 2 pointers

--- pointer.c
+++ pointer.c
@@ -12,21 +12,37 @@ int main(void)
     int x = 42;
     int y = 99;
     int *p = &x;
+    int *q = &x;
+
+    // p and q are separate variables.
+    // they don't point to each other.
+    // they both point to the same third thing: x
 
     printf("x = %d\n", x);
-    printf("address of x = %p\n", (void*)&x);
-    printf("p = %p\n", (void *)p);
     printf("*p = %d\n", *p);
+    printf("*q = %d\n", *q);
 
-    p = &y;
-    *p = 500;
-    p = &x;
+    *q = 100;
+    printf("\n");
 
     printf("x = %d\n", x);
-    printf("y = %d\n", y);
-    printf("address of y = %p\n", (void*)&y);
-    printf("p = %p\n", (void *)p);
     printf("*p = %d\n", *p);
+    printf("*q = %d\n", *q);
+
+    // printf("x = %d\n", x);
+    // printf("address of x = %p\n", (void*)&x);
+    // printf("p = %p\n", (void *)p);
+    // printf("*p = %d\n", *p);
+
+    // p = &y;
+    // *p = 500;
+    // p = &x;
+
+    // printf("x = %d\n", x);
+    // printf("y = %d\n", y);
+    // printf("address of y = %p\n", (void*)&y);
+    // printf("p = %p\n", (void *)p);
+    // printf("*p = %d\n", *p);
     
 
     // *p = 100;
